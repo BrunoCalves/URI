4ac17b2b033f6c62275e553c754f711feb720e1a
Bruno Alves <100779718+BrunoCalves@users.noreply.github.com>
2022-03-06 11:52:07 -0300

    Delete #1009 - Salário com Bônus.py

--- #1009 - Salário com Bônus.py
+++
@@ -1,9 +0,0 @@
-nome = input()
-salarioFixo = float(input())
-vendas = float(input())
-
-comissao = vendas * 0.15
-salarioBonus = salarioFixo + comissao
-
-
-print("TOTAL = R$ %.2F" % salarioBonus) 

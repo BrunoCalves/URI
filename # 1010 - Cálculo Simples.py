444559863a5b305dc3558250f35839d408fe1724
Bruno Alves <100779718+BrunoCalves@users.noreply.github.com>
2022-03-06 11:52:17 -0300

    Delete # 1010 - Cálculo Simples.py

--- # 1010 - Cálculo Simples.py
+++
@@ -1,6 +0,0 @@
-peca1 = input().split(" ")
-peca2 = input().split(" ")
-
-total = (float(peca1[1]) * float(peca1[2])) + (float(peca2[1]) * float(peca2[2]))
-
-print("VALOR A PAGAR: R$ %.2f" % total)
